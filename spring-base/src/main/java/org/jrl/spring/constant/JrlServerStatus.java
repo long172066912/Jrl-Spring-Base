b5c1aeaece9970d0371e9509d361ecc676e0929b
JerryLong <172066912@qq.com>
2024-12-17 15:02:56 +0800

    feat: 定义常量枚举

---
+++ spring-base/src/main/java/org/jrl/spring/constant/JrlServerStatus.java
@@ -0,0 +1,45 @@
+package org.jrl.spring.constant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+* 服务状态枚举
+* @author JerryLong
+*/
+public enum JrlServerStatus {
+    /**
+     * 准备阶段
+     */
+    PREPARE,
+    /**
+     * 运行中
+     */
+    RUNNING,
+    /**
+     * 停止运行
+     */
+    SHUTDOWN,
+    /**
+     * 异常
+     */
+    ABNORMAL;
+
+    private static Logger LOGGER = LoggerFactory.getLogger(JrlServerStatus.class);
+
+    private static JrlServerStatus jrlServerStatus = PREPARE;
+
+    public static void setStatus(JrlServerStatus status) {
+        if (null == status) {
+            return;
+        }
+        if (!jrlServerStatus.equals(status)) {
+            LOGGER.info("jrlServerStatus oldStatus : {} , newStatus : {}", jrlServerStatus, status);
+            jrlServerStatus = status;
+        }
+    }
+
+    public static JrlServerStatus getStatus() {
+        return jrlServerStatus;
+    }
+}
---
+++ spring-base/src/main/java/org/jrl/spring/constant/JrlSpringOrderConstants.java
@@ -0,0 +1,11 @@
+package org.jrl.spring.constant;
+
+/**
+* 顺序常量，0 ~ 100 ，数值大优先执行，默认10
+* @author JerryLong
+*/
+public class JrlSpringOrderConstants {
+
+    public static final int DEFAULT_MIDDLE = 10;
+
+}
